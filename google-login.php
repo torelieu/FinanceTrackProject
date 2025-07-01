9c3370206120c992f21a25145a4a11a41004defe
torelieu <simon1.navratil@gmail.com>
2025-07-01 12:55:48 +0200

    Update google-login.php

--- google-login.php
+++ google-login.php
@@ -10,7 +10,7 @@ require_once 'vendor/autoload.php'; // Načtení Google API knihovny
 $client = new Google_Client();
 $client->setClientId('699961376156-vgbq2pahgb84jon13vr2rq7lbrd7ambp.apps.googleusercontent.com'); // Nahraďte svým Client ID
 $client->setClientSecret('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'); // Nahraďte svým Client Secret
-$client->setRedirectUri('/google-callback.php'); // Nahraďte svou redirect URI
+$client->setRedirectUri('https://finance-track-uxdj.onrender.com/google-callback.php'); // Nahraďte svou redirect URI
 $client->addScope('email');
 $client->addScope('profile');
 
